1998f702e1be04590c6334d7dcff0fe38c7b1871
davronkhamdamov <xamdamovdavron6@gmail.com>
2022-09-06 08:51:26 +0500

    hello

---
+++ script.js
@@ -0,0 +1,45 @@
+// function dark() {
+//   document.getElementById('hero_dark').style.background = '#000';
+//   document.getElementById('text-dark').style.color = '#fff';
+//   document.getElementById('text-darker').style.color = '#fff';
+//   document.getElementById('text').style.color = '#fff';
+//   document.getElementById('text-dark').style.color = '#fff';
+//   document.getElementById('text-dark').style.color = '#fff';
+// }
+// function black() {
+//   document.getElementById('hero_dark').style.background = '#fff';
+//   document.getElementById('text-dark').style.color = '#000';
+//   document.getElementById('text-darker').style.color = '#000';
+//   document.getElementById('text').style.color = '#000';
+//   document.getElementById('text-dark').style.color = '#000';
+//   document.getElementById('text-dark').style.color = '#000';
+// }
+// let radius = 1;
+// let x = 1;
+// let y = 1;
+// const circle = {
+//   radius: 1,
+//   location: {
+//     x: 1,
+//     y: 1,
+//   },
+//   isVisible: true,
+//   draw: function () {
+//     console.log('doira');
+//   },
+// };
+// function draw() {}
+// function move() {}
+// circle.draw();
+let string = 'string...';
+console.log(string);
+console.log(string.charAt(1));
+console.log(string.concat('hello'));
+console.log(string.endsWith('.'));
+console.log(string.includes('ring'));
+console.log(string.indexOf('s'));
+console.log(string.length);
+console.log(string.replace('string...', string));
+console.log(string.slice(1));
+console.log(string.split(' '));
+console.log(string.slice(2, 5));
